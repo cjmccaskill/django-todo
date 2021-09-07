9597a878ac6e13aa1d2bef48d40c45ba6a3f3791
CJ McCaskill <cjmccaskill@gmail.com>
2021-09-07 11:33:54 -0500

    initial commit

---
+++ todoproject/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from rest_framework import routers
+from todos.views import TodoViewSet
+
+# create a new router
+router = routers.DefaultRouter()
+# register our viewsets
+router.register(r'todos', TodoViewSet) #register "/todos" routes
+
+urlpatterns = [
+    # add all of our router urls
+    path('', include(router.urls))
+]
